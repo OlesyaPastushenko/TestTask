cea526de5d375b718c6fcda40065392dd7c07f7c
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-04 14:22:15 +0100

    button mobile version added

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,11 +2,10 @@ import "./style.scss"
 import { useContext } from "react"
 import { InputContext } from "../../context/InputContext"
 
+  
 interface InputProps {
-   onSearch: (event: React.FormEvent<HTMLFormElement>)=> void
-}
-
-
+    onSearch: (event: any) => void
+  }
 export function Input({onSearch}: InputProps) {
 const {value, setValue}= useContext(InputContext)
 
@@ -14,11 +13,12 @@ return(
     
     <div className="inputMain">
     <div className="inputContainer">
-    <button className="inputSearchBtn" ></button>
+    <button className="inputSearchBtn"></button>
     <form className="inputForm" onSubmit={onSearch}>
     <input className="inputInput" type="text" value={value} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setValue(e.target.value)}}/>
     </form>
     </div>
+    <div className="searchBtn" onClick={onSearch}>Search</div>
     </div>
 )
 }
